1c7ce93cbaa42d419f8fc0f0def9db97b751fd61
Quentin ROCHE <quentinroche@msn.com>
2025-07-01 15:34:11 +0000

    FINAL 5

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -4,9 +4,10 @@ select
     o.orders_id,
     o.date_date,
     o.margin,
-    + s.shipping_fee,
-    - s.logcost,
-    - s.ship_cost as operational_margin
+    s.shipping_fee,
+    s.logcost,
+    s.ship_cost,
+    o.margin + s.shipping_fee - s.logcost - s.ship_cost as operational_margin
 from {{ ref('int_sales_margin') }} o
 left join {{ ref('stg_raw__ship') }} s
     on o.orders_id = s.orders_id
